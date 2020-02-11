fbbabc27890901554ad3b2a1a62685867cc1ed06
Nick Solonyy <nicksolony@gmail.com>
2020-02-11 02:37:04 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 require 'rake'
 require 'active_record'
 require 'yaml/store'
@@ -17,25 +17,3 @@ ActiveRecord::Base.establush_connection(
 )
 
 require_relative "../artist.rb"
-=======
-require 'rake'
-require 'active_record'
-require 'yaml/store'
-require 'ostruct'
-require 'date'
-
-
-require 'bundler/setup'
-Bundler.require
-
-
-# put the code to connect to the database here
-ActiveRecord::Base.establush_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite"
-)
-
-
-
-require_relative "../artist.rb"
->>>>>>> ef86806007091f5e9766f471b67744027fc5edd7
