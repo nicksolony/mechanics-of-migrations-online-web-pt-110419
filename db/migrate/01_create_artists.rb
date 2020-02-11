b209de3079e8cad660faad7e4d63ec875896a336
Nick Solonyy <nicksolony@gmail.com>
2020-02-11 02:36:43 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -16,22 +16,3 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   end
 
 end
-=======
-class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
-  end
-
-  def down
-  end
-
-  def change
-    create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
-    end
-  end
-
-end
->>>>>>> ef86806007091f5e9766f471b67744027fc5edd7
