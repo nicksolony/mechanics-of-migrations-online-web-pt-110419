d5669feb7985bc678d6b292d7482d92b5b756a0c
Nick Solonyy <nicksolony@gmail.com>
2020-02-11 02:46:30 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -1,17 +1,8 @@
 
-class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
-  end
-
-  def down
-  end
-
-  def change
-    create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
+  
+    def change
+      add_column :artists, :favorite_food, :string
     end
   end
 
